e36793f9d1b4fa54eefb3af6ce50a14079fbc873
Sandeep Kumar Mishra <sk2011mishra@gmail.com>
2022-10-27 22:39:55 +0530

    lexical processing NLP

--- NLP/lexicalProcessing.py
+++ NLP/lexicalProcessing.py
@@ -31,10 +31,8 @@ wordnet_lemmatizer = WordNetLemmatizer()
 # Preprocess document and convert it to its stem or lemma
 def preprocess(document, stem=True):
     'changes document to lower case and removes stopwords'
-
     # change sentence to lower case
     document = document.lower()
-
     # tokenize into words
     words = word_tokenize(document)
 
@@ -63,6 +61,7 @@ def count_vectors(document, bow=True, tfidf=True):
         bow_model = vectorizer.fit_transform(document)
         vectorizer = TfidfVectorizer()
         tfidf_model = vectorizer.fit_transform(document)
+        #tfidf_model.to_array()
         return (bow_model, tfidf_model)
     else:
         vectorizer = TfidfVectorizer()
@@ -70,6 +69,39 @@ def count_vectors(document, bow=True, tfidf=True):
         return tfidf_model
 
 # Step 4: Advance Lexical Processing
+
+  # soundex
+def get_soundex(token, dictionary=None):
+    """Get the soundex code for the string"""
+    token = token.upper()
+
+    soundex = ""
+    
+    # first letter of input is always the first letter of soundex
+    soundex += token[0]
+    
+    # create a dictionary which maps letters to respective soundex codes. Vowels and 'H', 'W' and 'Y' will be represented by '.'
+    if dictionary !=None:
+        pass
+    else: 
+        dictionary = {"BFPV": "1", "CGJKQSXZ":"2", "DT":"3", "L":"4", "MN":"5", "R":"6", "AEIOUHWY":"."}
+
+    for char in token[1:]:
+        for key in dictionary.keys():
+            if char in key:
+                code = dictionary[key]
+                if code != soundex[-1]:
+                    soundex += code
+
+    # remove vowels and 'H', 'W' and 'Y' from soundex
+    soundex = soundex.replace(".", "")
+    
+    # trim or pad to make soundex a 4-character code
+    soundex = soundex[:4].ljust(4, "0")
+        
+    return soundex
+
+   # Edit Distance
 def lev_distance(source='', target=''):
     """Make a Levenshtein Distances Matrix"""
     
